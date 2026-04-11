ccaccdd5e565f91687deae1e3e30b740a6727149
pavl-g <scrappers.tm@gmail.com>
2026-04-11 10:42:20 +0200

    build.sh: added build for new dot diagrams

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 cached_work_dir=$(pwd)
 cd ./modules/COMPANION/assets
 dot -Tsvg ./dot/atls_xabcde_algorithm.dot -o ./images/xabcde/atls.svg
+dot -Tsvg ./dot/medical_listening.dot -o ./images/medical-listening.svg
+dot -Tsvg ./dot/medical_writing.dot -o ./images/medical-writing.svg
 cd $cached_work_dir
 
 npx antora antora-playbook.yml
